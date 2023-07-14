5a6dc6c7cca513cf0fd6ed97e9f4b6bd081bbff4
pgourishankar07 <100337318+pgourishankar07@users.noreply.github.com>
2023-07-14 15:37:49 +0530

    Fetching data from TVMAZE api on dynamic search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Home() {
+  const [val, setVal] = useState('');
+  function change(event) {
+    setVal(event.target.value);
+  }
+  async function Search(event) {
+    event.preventDefault();
+    const res = await fetch(`https://api.tvmaze.com/search/shows?q=${val}`);
+    const body = await res.json();
+    console.log(body);
+  }
   return (
     <div>
       <h1>Hello</h1>
       <Link to="/contact">Go to Contact page</Link>
+      <form onSubmit={Search}>
+        <input type="text" onChange={change} value={val}></input>
+        <button type="submit">Search</button>
+      </form>
     </div>
   );
 }
