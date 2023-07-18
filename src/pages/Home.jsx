9d224079211baa951ce59f2299d4487aafbfa0f2
pgourishankar07 <100337318+pgourishankar07@users.noreply.github.com>
2023-07-18 10:37:02 +0530

    Added useReducer concept in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,42 @@
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { searchShowsActors } from './../api/tvmazeData';
 import SearchForm from '../components/SearchForm';
 import ShowsGrid from '../components/shows/ShowsGrid';
 import ActorsGrid from '../components/actors/ActorsGrid';
+//____
+
+function reducerFn(currCount, action) {
+  //action is whatever we dispatch as an object
+  switch (action.type) {
+    case 'INCREMENT':
+      return currCount + 1;
+    case 'DECREMENT':
+      return currCount - 1;
+    case 'RESET':
+      return 0;
+    case 'SET':
+      return action.newVal;
+  }
+  return 0;
+}
 
 function Home() {
+  const [counter, dispatch] = useReducer(reducerFn, 0);
+  function inc() {
+    dispatch({ type: 'INCREMENT' });
+  }
+  function dec() {
+    dispatch({ type: 'DECREMENT' });
+  }
+  function rst() {
+    dispatch({ type: 'RESET' });
+  }
+  function set50() {
+    dispatch({ type: 'SET', newVal: 50 });
+  }
+  //____
+
   const [filter, setFilter] = useState(null);
 
   const { data: apiData, error: apiErr } = useQuery({
@@ -14,7 +45,7 @@ function Home() {
       return searchShowsActors(filter.val, filter.srchOpt);
     },
     enabled: !!filter,
-    refetchOnWindowFocus: false, //so that everyTime u focus on windows it will not fetch data
+    refetchOnWindowFocus: false,
   });
 
   async function Search(val, srchOpt) {
@@ -43,6 +74,21 @@ function Home() {
   return (
     <>
       <SearchForm SearchData={Search} />
+
+      <div>Counter : {counter}</div>
+      <button type="button" onClick={inc}>
+        Increment
+      </button>
+      <button type="button" onClick={dec}>
+        Decrement
+      </button>
+      <button type="button" onClick={rst}>
+        Reset
+      </button>
+      <button type="button" onClick={set50}>
+        Set to 50
+      </button>
+
       <div>{renderData()}</div>
     </>
   );
